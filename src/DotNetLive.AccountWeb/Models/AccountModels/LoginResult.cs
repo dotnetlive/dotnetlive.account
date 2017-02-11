7d11210d087fe33b9cf2c055214a3dd83e225a6f
Duke Cheng <dk@feinian.me>
2017-02-11 17:24:38 +0800

    added account api

---
+++ src/DotNetLive.AccountWeb/Models/AccountModels/LoginResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetLive.AccountWeb.Models.AccountModels
+{
+    public class LoginResult
+    {
+        public string Token { get; set; }
+    }
+}
---
+++ src/DotNetLive.AccountWeb/Models/ErrorResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetLive.AccountWeb.Models
+{
+    public class ErrorResponse
+    {
+        public string Message { get; set; }
+    }
+}
