61f511a8b267d60b9923352f9c04e7241dca6340
Duke Cheng <dk@feinian.me>
2017-02-12 16:16:37 +0800

    update gulpconfig

--- src/AccountWeb/DotNetLive.AccountWeb/gulpfile.js
+++ src/AccountWeb/DotNetLive.AccountWeb/gulpfile.js
@@ -5,42 +5,54 @@ var gulp = require("gulp"),
     rimraf = require("rimraf"),
     concat = require("gulp-concat"),
     cssmin = require("gulp-cssmin"),
-    uglify = require("gulp-uglify");
+    uglify = require("gulp-uglify"),
+    es = require('event-stream');
 
-var paths = {
-    defaultwebroot: "./wwwroot/default/",
-    defaultweb: {},
-};
-
-paths.defaultweb.js = paths.defaultwebroot + "js/**/*.js";
-paths.defaultweb.minJs = paths.defaultwebroot + "js/**/*.min.js";
-paths.defaultweb.css = paths.defaultwebroot + "css/**/*.css";
-paths.defaultweb.minCss = paths.defaultwebroot + "css/**/*.min.css";
-paths.defaultweb.concatJsDest = paths.defaultwebroot + "js/site.min.js";
-paths.defaultweb.concatCssDest = paths.defaultwebroot + "css/site.min.css";
-
-gulp.task("clean:js", function (cb) {
-    rimraf(paths.defaultweb.concatJsDest, cb);
+var areas = ["default"];
+var bundles = areas.map(function (obj) {
+    var webroot = "./wwwroot/" + obj + "/";
+    return {
+        js: webroot + "js/**/*.js",
+        minJs: webroot + "js/**/*.min.js",
+        css: webroot + "css/**/*.css",
+        minCss: webroot + "css/**/*.min.css",
+        concatJsDest: webroot + "js/site.min.js",
+        concatCssDest: webroot + "css/site.min.css",
+    };
 });
 
-gulp.task("clean:css", function (cb) {
-    rimraf(paths.defaultweb.concatCssDest, cb);
+gulp.task('clean:js', function () {
+    bundles.map(function (obj) {
+        rimraf(obj.concatJsDest, (error) => { });
+    });
+});
+
+gulp.task('clean:css', function () {
+    bundles.map(function (obj) {
+        rimraf(obj.concatCssDest, (error) => { });
+    });
 });
 
 gulp.task("clean", ["clean:js", "clean:css"]);
 
-gulp.task("min:js", function () {
-    gulp.src([paths.defaultweb.js, "!" + paths.defaultweb.minJs], { base: "." })
-        .pipe(concat(paths.defaultweb.concatJsDest))
-        .pipe(uglify())
-        .pipe(gulp.dest("."));
+gulp.task('min:js', function () {
+    return es.merge(bundles.map(function (obj) {
+        return gulp.src([obj.js, "!" + obj.minJs], { base: "." })
+            .pipe(concat(obj.concatJsDest))
+            .pipe(uglify())
+            .pipe(gulp.dest("."));
+    }));
 });
 
-gulp.task("min:css", function () {
-    gulp.src([paths.defaultweb.css, "!" + paths.defaultweb.minCss])
-        .pipe(concat(paths.defaultweb.concatCssDest))
-        .pipe(cssmin())
-        .pipe(gulp.dest("."));
+gulp.task('min:css', function () {
+    return es.merge(bundles.map(function (obj) {
+        return gulp.src([obj.css, "!" + obj.minCss])
+            .pipe(concat(obj.concatCssDest))
+            .pipe(cssmin())
+            .pipe(gulp.dest("."));
+    }));
 });
 
-gulp.task("default", ["min:js", "min:css"]);
+gulp.task("min", ["min:js", "min:css"]);
+
+gulp.task("default", ["clean", "min"]);
