d00872846d80eb02f808f1152f190aaaa01c35e7
919849902@qq.com <919849902@qq.com>
2017-02-15 23:52:03 +0800

    add login,router

--- src/accountweb-usercenter-vue/src/routers.js
+++ src/accountweb-usercenter-vue/src/routers.js
@@ -3,11 +3,25 @@ module.exports = {
   routes: [
     {
       path: '/',
-      component: resolve => require(['./view/Home.vue'], resolve)
+      component: resolve => require(['./view/Login.vue'], resolve)
     },
     {
-      path: '/profile',
-      component: resolve => require(['./view/Profile.vue'], resolve)
+      path: '/home',
+      component: resolve => require(['./view/Home.vue'], resolve),
+      children: [
+        {
+          // 当 /user/:id/profile 匹配成功，
+          // UserProfile 会被渲染在 User 的 <router-view> 中
+          path: 'index',
+          component: resolve => require(['./components/Index.vue'], resolve)
+        },
+        {
+          // 当 /user/:id/posts 匹配成功
+          // UserPosts 会被渲染在 User 的 <router-view> 中
+          path: 'profile',
+          component: resolve => require(['./components/Profile.vue'], resolve)
+        }
+      ]
     }
   ]
 }
