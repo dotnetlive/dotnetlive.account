07bf9fe749a2445194e212d732a70ec25a885fe5
919849902@qq.com <919849902@qq.com>
2017-02-15 22:17:51 +0800

    add ui

---
+++ src/accountweb-usercenter-vue/src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App'
+import routers from './routers'
+import './assets/content/bootstrap.min.css'
+import './assets/content/animate.css'
+import './assets/content/style.css'
+/* eslint-disable no-new */
+Vue.use(VueRouter)
+const router = new VueRouter(routers)
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
