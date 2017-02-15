34e174ea22ad1c634fe39a54a36382d7f4573fad
919849902@qq.com <919849902@qq.com>
2017-02-15 23:56:37 +0800

    update router

--- src/accountweb-usercenter-vue/src/routers.js
+++ src/accountweb-usercenter-vue/src/routers.js
@@ -10,14 +10,10 @@ module.exports = {
       component: resolve => require(['./view/Home.vue'], resolve),
       children: [
         {
-          // 当 /user/:id/profile 匹配成功，
-          // UserProfile 会被渲染在 User 的 <router-view> 中
           path: 'index',
           component: resolve => require(['./components/Index.vue'], resolve)
         },
         {
-          // 当 /user/:id/posts 匹配成功
-          // UserPosts 会被渲染在 User 的 <router-view> 中
           path: 'profile',
           component: resolve => require(['./components/Profile.vue'], resolve)
         }
